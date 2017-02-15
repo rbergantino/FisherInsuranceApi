41a87f566881dd3446cb6e625c19d02cb3720f17
Ross Bergantino <Ross Bergantino>
2017-02-15 18:13:53 -0500

    exercise 0

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using FisherInsuranceApi.Data;
 
 namespace FisherInsuranceApi
 {
@@ -29,6 +30,7 @@ namespace FisherInsuranceApi
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSingleton<IMemoryStore, MemoryStore>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
