f85b72972f1d57f54a72fd99b1d9c0b4574f3c72
Ross Bergantino <Ross Bergantino>
2017-03-28 19:00:51 -0400

    exercise 4

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -13,7 +13,8 @@ import { ClaimsComponent } from './components/claims/claims.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
 
 import { AppRouting } from './app.routing';
-
+import { AuthHttp } from "./auth.http";
+import { AuthService } from "./auth.service";
 
 @NgModule({
     declarations: [
@@ -30,10 +31,11 @@ import { AppRouting } from './app.routing';
         FormsModule,
         ReactiveFormsModule,
         RouterModule,
-        AppRouting 
+        AppRouting
     ],
     providers: [
-
+        AuthService,
+        AuthHttp
     ],
     bootstrap: [
         AppComponent
