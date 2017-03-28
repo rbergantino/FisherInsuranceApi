589276c1f57a77426b32df6329f72215d929db3b
Ross Bergantino <Ross Bergantino>
2017-03-27 20:18:44 -0400

    finished exercise 3; corrected filenames

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,25 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'rxjs/Rx';
-// import our application component
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.components'; 
-import { NavBarComponent } from './components/navbar/navbar.components';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClaimsComponent } from './components/claims/claims.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+
+import { AppRouting } from './app.routing';
+
+
 @NgModule({
- declarations: [
- AppComponent,
- HomeComponent,
- NavBarComponent 
- ],
- imports: [
- BrowserModule,
- HttpModule
- ],
- providers: [ 
- ],
- bootstrap: [
- AppComponent
- ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        NavBarComponent,
+        LoginComponent,
+        ClaimsComponent,
+        QuotesComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AppRouting 
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+        AppComponent
+    ],
 })
 export class AppModule { }
